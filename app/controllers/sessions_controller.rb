7f161fe18385a1fe25b647384f235bc35fb47bf7
Daniel Rodriguez <drod2793@gmail.com>
2017-01-30 15:54:13 -0700

    Adds duration to omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def create
-    byebug
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, "ynrkyFVZ-Gm4SQ", "	aORc_ep3s3nETsbgeHO2Tn3yVbI", scope: 'identity'
+  provider :reddit, "ynrkyFVZ-Gm4SQ", "aORc_ep3s3nETsbgeHO2Tn3yVbI", scope: 'identity', duration: 'temporary'
 end
