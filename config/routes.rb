1da8ac42a888c3c8735c95be89101e7b28426ba3
Daniel Rodriguez <drod2793@gmail.com>
2017-01-30 15:21:57 -0700

    Adds scope to omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/auth/reddit', as: :reddit_login
-  get 'users/auth/reddit/callback', to: 'sessions#create'
+  get '/auth/reddit/callback', to: 'sessions#create'
 end
