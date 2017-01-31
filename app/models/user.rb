c461d496049822880a87a98291d2a60777d73e7c
Daniel Rodriguez <drod2793@gmail.com>
2017-01-30 20:41:06 -0700

    Adds validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :name, :uid, :token, presence: true
+  validates :name, :uid, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,10 +8,26 @@ RSpec.describe User, :type => :model do
       expect(user).to be_invalid
     end
 
+    it "has a unique name" do
+      user_1 =  User.create(name: "some_name", uid: "some_uid", token: "some_token")
+      user_2 = User.new(name: "some_name", uid: "different_uid", token: "different_token")
+
+      expect(user_1).to be_valid
+      expect(user_2).to be_invalid
+    end
+
     it "is not valid without a uid" do
       user = User.new(name: "some_name", token: "some_token")
 
       expect(user).to be_invalid
+    end
+
+    it "has a unique uid" do
+      user_1 =  User.create(name: "some_name", uid: "some_uid", token: "some_token")
+      user_2 = User.new(name: "different_name", uid: "some_uid", token: "different_token")
+
+      expect(user_1).to be_valid
+      expect(user_2).to be_invalid
     end
 
     it "is not valid without a token" do
@@ -23,7 +39,7 @@ RSpec.describe User, :type => :model do
     it "is valid with all attributes" do
       user = User.new(name: "some_name", uid: "some_uid", token: "some_token")
 
-      expect(user).to be_invalid
+      expect(user).to be_valid
     end
   end
 end
