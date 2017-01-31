f469ead651c34c50662eebc39bba8375991fa0a7
Daniel Rodriguez <drod2793@gmail.com>
2017-01-30 20:16:31 -0700

    Adds user model and migration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    byebug
+    name = auth_hash["info"]["name"]
+    auth_hash["uid"]
+    token = auth_hash["credentials"]["token"]
   end
 
   protected
