71440bd210ae0dc4d92ac8c964f5bd0fe4ef0769
Daniel Rodriguez <drod2793@gmail.com>
2017-02-01 11:25:49 -0700

    updates login test with vcr

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -3,25 +3,29 @@ require 'rails_helper'
 describe "When a user logs in" do
 
   it "they can see their information" do
-    visit root_path
+    VCR.use_cassette("login") do
+      visit root_path
 
-    stub_omniauth
-    click_on("Login with Reddit")
+      stub_omniauth
+      click_on("Login with Reddit")
 
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have("Name: some_name")
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Name: drod2793")
+      expect(page).to have_content("Link Karma: 1")
+      expect(page).to have_content("Comment Karma: 0")
+    end
   end
 
   def stub_omniauth
     OmniAuth.config.test_mode = true
     OmniAuth.config.mock_auth[:reddit] = OmniAuth::AuthHash.new({
       provider: 'reddit',
-      uid: '29sk30',
+      uid: '14xe5j',
       info: {
-        name: 'some_name'
+        name: 'drod_2793'
       },
       credentials: {
-        token: 'rteQFePx-xaueLBTi6RZLvKuHSU'
+        token: 'OsSn1vW7nKdD3zM5N4n0sfV1zY8'
       }
       })
   end
