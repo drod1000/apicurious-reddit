4a0b60e34df305f6240db8ec83f5bae46ab70460
Daniel Rodriguez <drod2793@gmail.com>
2017-01-31 19:09:14 -0700

    Adds additional scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :reddit, ENV["REDDIT_CLIENT_ID"], ENV["REDDIT_SECRET"], scope: 'identity', duration: 'temporary'
+  provider :reddit, ENV["REDDIT_CLIENT_ID"], ENV["REDDIT_SECRET"], scope: 'identity,mysubreddits,read', duration: 'permanent'
 end
