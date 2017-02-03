e4ce2e10d8d7f681d63687579496ee108d162b22
Daniel Rodriguez <drod2793@gmail.com>
2017-02-02 19:10:40 -0700

    Refactors response parsing and symbolization

--- app/services/reddit_service.rb
+++ app/services/reddit_service.rb
@@ -1,36 +1,35 @@
 class RedditService
 
-  def self.raw_response(token, path)
+  def self.response(token, path)
     conn = Faraday.new(:url => "https://oauth.reddit.com#{path}") do |faraday|
       faraday.headers[:Authorization] = "bearer #{token}"
       faraday.adapter  Faraday.default_adapter
     end
-    conn.get
+    parse_and_symbolize(conn.get)
+  end
+
+  def self.parse_and_symbolize(raw_response)
+    JSON.parse(raw_response.body, symbolize_names: true)
   end
 
   def self.get_user_info(token)
-    raw_data = raw_response(token, "/api/v1/me/")
-    JSON.parse(raw_data.body, symbolize_names: true)
+    response(token, "/api/v1/me/")
   end
 
   def self.get_user_subreddits(token)
-    raw_data = raw_response(token, "/subreddits/mine/subscriber")
-    JSON.parse(raw_data.body, symbolize_names: true)[:data][:children]
+    response(token, "/subreddits/mine/subscriber")[:data][:children]
   end
 
   def self.get_subreddit_rules(token, name)
-    raw_data = raw_response(token, "/r/#{name}/about/rules")
-    JSON.parse(raw_data.body, symbolize_names: true)[:rules]
+    response(token, "/r/#{name}/about/rules")[:rules]
   end
 
   def self.get_single_subreddit(token, permalink)
-    raw_data = raw_response(token, "/#{permalink}?type=t3&limit=15")
-    JSON.parse(raw_data.body, symbolize_names: true)[:data][:children]
+    response(token, "/#{permalink}?type=t3&limit=15")[:data][:children]
   end
 
   def self.get_single_subreddit_post(token, permalink)
-    raw_data = raw_response(token, "/#{permalink}")
-    JSON.parse(raw_data.body, symbolize_names: true)
+    response(token, "/#{permalink}")
   end
 
 end
