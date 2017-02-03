66562c6daf87937d5d8e273f04e075d69eb26e6b
Daniel Rodriguez <drod2793@gmail.com>
2017-02-02 19:34:31 -0700

    Refactors reddit subreddit with posts

--- app/models/reddit_subreddit_with_posts.rb
+++ app/models/reddit_subreddit_with_posts.rb
@@ -1,18 +1,22 @@
 class RedditSubredditWithPosts
   attr_reader :name, :posts, :rules
 
-  def initialize(token, attrs)
-    @name = attrs[0][:data][:subreddit]
-    @posts = attrs.map do |post|
-      RedditPost.new(post)
-    end
-    @rules ||= RedditService.get_subreddit_rules(token, @name).map do |rule|
-      rule[:description]
-    end
+  def initialize(token, data)
+    @name = data[0][:data][:subreddit]
+    @posts = get_posts(data)
+    @rules ||= get_rules(token, @name)
   end
 
   def self.get_single_subreddit(token, name)
     json = RedditService.get_single_subreddit(token, name)
     RedditSubredditWithPosts.new(token, json)
+  end
+
+  def get_posts(data)
+    data.map{ |post| RedditPost.new(post)}
+  end
+
+  def get_rules(token, name)
+    RedditService.get_subreddit_rules(token, name).map { |rule| rule[:description] }
   end
 end
